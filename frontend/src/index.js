b1a272ee110c2c032293c5737a2f8b8e61b6e0a3
nicholaKim <86nichola@gmail.com>
2022-01-27 23:55:00 +0900

    add loaders for css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./containers/Main";
+import "./index.css";
 
 const App = () => {
   return (
@@ -8,22 +9,30 @@ const App = () => {
       <Main>
         <div>
           <header>
+            HEADER
             <nav>
-              <a></a>
+              Navigator
+              <a>LINK</a>
               <div>
                 <ul>
-                  <li></li>
+                  <li>LIST_ITEM</li>
                 </ul>
                 <ul>
-                  <li></li>
+                  <li>LIST_ITEM</li>
                 </ul>
                 <ul>
-                  <li></li>
+                  <li>LIST_ITEM</li>
                 </ul>
               </div>
             </nav>
           </header>
         </div>
+        <section>SECTION_1</section>
+        <section>SECTION_2</section>
+        <section>SECTION_3</section>
+        <section>SECTION_4</section>
+        <section>SECTION_5</section>
+        <footer>FOOTER</footer>
       </Main>
       <h1>Hello dodo World!</h1>
     </>
