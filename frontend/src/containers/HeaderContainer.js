d1c51ab9e863dee5a3607c59a0967fa2247fb924
nicholaKim <86nichola@gmail.com>
2022-02-10 23:45:16 +0900

    work 2

--- frontend/src/containers/HeaderContainer.js
+++ frontend/src/containers/HeaderContainer.js
@@ -3,16 +3,20 @@ import Header from "components/Header";
 import Navigator from "components/Navigator";
 import AuthContainer from "./AuthContainer";
 import Introduce from "components/Introduce";
+import Dodoist from "../components/Dodoist";
+import { Link } from "react-router-dom";
 
 function HeaderContainer() {
   return (
     <div>
       <Header>
         <Navigator>
-          <a>dodo</a>
+          <Link to="/">
+            <Dodoist />
+          </Link>
+
           <div>
             <Introduce />
-            <AuthContainer />
             <AuthContainer />
           </div>
         </Navigator>
