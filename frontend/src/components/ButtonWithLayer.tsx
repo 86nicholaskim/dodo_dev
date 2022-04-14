70559d8bcf756b5d8ec1f836a2da5aa221f48260
nicholaKim <86nichola@gmail.com>
2022-04-14 22:32:40 +0900

    layer button

--- frontend/src/components/ButtonWithLayer.tsx
+++ frontend/src/components/ButtonWithLayer.tsx
@@ -5,12 +5,6 @@ import Icon from './Icon'
 
 const StyledOlDiv = styled.div`
     padding-top: var(--space-4);
-    :hover {
-        visibility: visible;
-        opacity: 1;
-        transform: translateY(0);
-        pointer-events: auto;
-    }
 `
 const StyledOl = styled.ol`
     position: absolute;
@@ -30,23 +24,35 @@ const StyledOl = styled.ol`
     min-width: 350px;
 `
 const StyledLi = styled.li`
-    margin: var(--space-4) 0;
+    --curr-line-height: var(--body-md-line-height);
+    --curr-letter-spacing: var(--body-md-letter-spacing);
+    --curr-font-weight: var(--body-md-font-weight);
+    --curr-text-transform: var(--body-md-text-transform);
+
+    line-height: var(--curr-line-height);
+    letter-spacing: var(--curr-letter-spacing);
+    font-weight: var(--curr-font-weight);
+    text-transform: var(--curr-text-transform);
+    :hover {
+        text-decoration: none;
+        color: var(--color-grey-08);
+        background: var(--color-grey-01);
+    }
 `
 
 const StyledLink = styled(Link)`
     display: flex;
     align-items: center;
     padding: var(--space-16) var(--space-24);
-    font-size: var(--curr-font-size);
+
     line-height: var(--curr-line-height);
     letter-spacing: var(--curr-letter-spacing);
     font-weight: var(--curr-font-weight);
     text-transform: var(--curr-text-transform);
-    --curr-font-size: var(--body-md-font-size);
-    --curr-line-height: var(--body-md-line-height);
-    --curr-letter-spacing: var(--body-md-letter-spacing);
-    --curr-font-weight: var(--body-md-font-weight);
-    --curr-text-transform: var(--body-md-text-transform);
+
+    :hover {
+        text-decoration: none;
+    }
 `
 const StyledButtonDiv = styled.div`
     position: relative;
@@ -54,6 +60,12 @@ const StyledButtonDiv = styled.div`
     margin: 0;
     padding: 0;
     border: 0;
+    &:hover ${StyledOl} {
+        visibility: visible;
+        opacity: 1;
+        transform: translateY(0);
+        pointer-events: auto;
+    }
 `
 const StyledSpanDiv = styled.div`
     display: flex;
@@ -93,7 +105,26 @@ const StyledSpan2 = styled.span`
     color: var(--color-grey-08);
     margin-bottom: var(--space-4);
     margin-left: var(--space-8);
+    margin-bottom: 0;
 `
+
+const StyledSpan3 = styled.span`
+    text-align: 'left';
+
+    --curr-font-size: var(--body-md-font-size);
+    --curr-line-height: var(--body-md-line-height);
+    --curr-letter-spacing: var(--body-md-letter-spacing);
+    --curr-font-weight: var(--body-md-font-weight);
+    --curr-text-transform: var(--body-md-text-transform);
+    font-size: var(--curr-font-size);
+    line-height: var(--curr-line-height);
+    letter-spacing: var(--curr-letter-spacing);
+    font-weight: var(--curr-font-weight);
+    text-transform: var(--curr-text-transform);
+    padding-left: var(--space-12);
+    margin-bottom: 0;
+`
+
 const StyledSvg = styled(Icon)`
     fill: 'currentColor';
 
@@ -102,28 +133,26 @@ const StyledSvg = styled(Icon)`
     margin-left: var(--space-4);
     transition: var(--easing-smooth) var(--duration-slow);
 `
+const StyledHr = styled.hr`
+    margin-block-start: var(--spacing);
+    margin-block-end: var(--spacing);
+    height: 1px;
+    border: 0;
+
+    background-color: var(--color-grey-04);
+    margin: var(--space-4) var(--space-24);
+`
 
 interface buttonProps {
     label?: string
     linkList?: string[]
+    isVisible?: boolean
+    hideLayerList?: any
 }
 
 function ButtonWithLayer({ label, linkList }: buttonProps) {
-    const [visibleState, setVisible] = useState<boolean>(false)
-
-    const showLayerList = (e: SyntheticEvent<HTMLDivElement>) => {
-        setVisible(true)
-    }
-
-    const hideLayerList = (e: SyntheticEvent<HTMLDivElement>) => {
-        setVisible(false)
-    }
-
     return (
-        <StyledButtonDiv
-            onMouseEnter={showLayerList}
-            onMouseLeave={hideLayerList}
-        >
+        <StyledButtonDiv>
             <StyledButton>
                 <StyledSpan1>{label ? label : '리소스'}</StyledSpan1>
                 <StyledSvg viewBox="0 0 24 24" width="24" height="20">
@@ -135,34 +164,65 @@ function ButtonWithLayer({ label, linkList }: buttonProps) {
                 </StyledSvg>
             </StyledButton>
             <StyledOlDiv>
-                <StyledOl
-                    className="myLayerList"
-                    style={{
-                        visibility: visibleState ? 'visible' : 'hidden',
-                    }}
-                >
+                <StyledOl>
                     <StyledLi>
                         <StyledLink to="">
                             <StyledSpanDiv>
                                 <StyledSpan2>통합</StyledSpan2>
-                                <StyledSpan2>
+                                <StyledSpan3>
                                     dodoist를 IFTTT, 알렉사, 구글 캘린더 등과
                                     같은 툴과 연결시키세요...
-                                </StyledSpan2>
+                                </StyledSpan3>
                             </StyledSpanDiv>
                         </StyledLink>
+                        <StyledHr />
                     </StyledLi>
                     <StyledLi>
-                        dodoist를 즉시 실행하기 위해 알아야 할 모든 것이
-                        있습니다.
+                        <StyledLink to="">
+                            <StyledSpanDiv>
+                                <StyledSpan2>시작하기 가이드</StyledSpan2>
+                                <StyledSpan3>
+                                    dodoist를 즉시 실행하기 위해 알아야 할 모든
+                                    것이 있습니다.
+                                </StyledSpan3>
+                            </StyledSpanDiv>
+                        </StyledLink>
+                        <StyledHr />
                     </StyledLi>
                     <StyledLi>
-                        dodoist를 최대한으로 활용하기 위한 질문 답변 및 팁을
-                        찾으세요.
+                        <StyledLink to="">
+                            <StyledSpanDiv>
+                                <StyledSpan2>도움말 센터</StyledSpan2>
+                                <StyledSpan3>
+                                    dodoist를 최대한으로 활용하기 위한 질문 답변
+                                    및 팁을 찾으세요.
+                                </StyledSpan3>
+                            </StyledSpanDiv>
+                        </StyledLink>
+                        <StyledHr />
                     </StyledLi>
                     <StyledLi>
-                        가장 인기 있는 생산성 방법론을 알아보고 당신에게 가장
-                        적합한 것을 찾으세요.
+                        <StyledLink to="">
+                            <StyledSpanDiv>
+                                <StyledSpan2>생산선 + 방법론</StyledSpan2>
+                                <StyledSpan3>
+                                    가장 인기 있는 생산성 방법론을 알아보고
+                                    당신에게 가장 적합한 것을 찾으세요.
+                                </StyledSpan3>
+                            </StyledSpanDiv>
+                        </StyledLink>
+                        <StyledHr />
+                    </StyledLi>
+                    <StyledLi>
+                        <StyledLink to="">
+                            <StyledSpanDiv>
+                                <StyledSpan2>블로그</StyledSpan2>
+                                <StyledSpan3>
+                                    다른 곳에서 찾아 볼 수 없는 생산성 조언,
+                                    그리고 Todoist 팁 및 제품 소식이 있습니다.
+                                </StyledSpan3>
+                            </StyledSpanDiv>
+                        </StyledLink>
                     </StyledLi>
                 </StyledOl>
             </StyledOlDiv>
