46a96c9757f0bbc13a55d3bedaf257950b266cfb
nicholaKim <86nichola@gmail.com>
2022-09-15 06:49:13 +0900

    audit fix

--- frontend/src/00.oldJS/components/AuthLogin.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-function AuthLogin() {
-  return <Link to="/users/showlogin">로그인</Link>;
-}
-
-export default AuthLogin;
--- frontend/src/00.oldJS/components/AuthRegister.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-function AuthRegister() {
-  return <Link to="/users/showregister">가입</Link>;
-}
-
-export default AuthRegister;
